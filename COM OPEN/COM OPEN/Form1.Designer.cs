4fd29868c37934633a081baf0805436b947564a4
lithospheree <144734180+lithospheree@users.noreply.github.com>
2025-06-03 00:43:20 -0500

    Added all files to commit

---
+++ COM OPEN/COM OPEN/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace COM_OPEN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.formsPlot2 = new ScottPlot.WinForms.FormsPlot();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(257, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // formsPlot2
+            // 
+            this.formsPlot2.DisplayScale = 0F;
+            this.formsPlot2.Location = new System.Drawing.Point(12, 39);
+            this.formsPlot2.Name = "formsPlot2";
+            this.formsPlot2.Size = new System.Drawing.Size(776, 399);
+            this.formsPlot2.TabIndex = 1;
+            this.formsPlot2.Load += new System.EventHandler(this.formsPlot2_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.formsPlot2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private ScottPlot.WinForms.FormsPlot formsPlot2;
+    }
+}
+
---
+++ COM OPEN/COM OPEN/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Management;
+using System.Windows.Forms.VisualStyles;
+using ScottPlot.WinForms;
+using ScottPlot;
+using ScottPlot.Plottables;
+
+
+namespace COM_OPEN
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            populatePorts();
+
+            loggerTest();
+        }
+
+        private void populatePorts()
+        {
+
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                string[] portnames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+                var portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
+
+                foreach(string s in portList)
+                {
+                    comboBox1.Items.Add(s);
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string portName = comboBox1.SelectedItem.ToString();
+
+            string[] portnames = SerialPort.GetPortNames();
+
+            string port = "";
+
+            foreach (string s in portnames) 
+            {
+                if (portName.Contains(s))
+                {
+                    port = s; break;
+                }
+            }
+
+            try
+            {
+                SerialPort serialPort = new SerialPort(port, 921600, Parity.None, 8, StopBits.One);
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                serialPort.Open();
+                
+                serialPort.Write("Hello World!");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+                
+        }
+
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string data = sp.ReadLine();
+            string[] dataSplit = data.Select(s => s.ToString()).ToArray();
+
+        }
+
+        private void updateLogger(int id, int value)
+        {
+
+        }
+
+        private void formsPlot2_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void loggerTest()
+        {
+            var logger = formsPlot2.Plot.Add.DataLogger();
+            for (int i = 0; i < 100000; i++)
+            {
+                double y = Generate.RandomWalker.Next();
+
+                logger.Add(i, y);
+            }
+            formsPlot2.Refresh();
+        }
+    }
+}
